deed190f67a5097d3cf96fc0db285cad01b57ebb
VishalHalnawar <113388100+VishalHalnawar@users.noreply.github.com>
2023-10-18 11:45:00 +0530

    changed url paths

--- src/pages/posts/[id]/index.js
+++ src/pages/posts/[id]/index.js
@@ -34,7 +34,8 @@ const NovelEditor = () => {
     if(id){
       try {
         const response = await fetch(
-          `http://localhost:3000/api/singleuser?userId=${id}`
+          
+          `https://cw-webadmin.vercel.app/api/singleuser?userId=${id}`
         );
         if (!response.ok) {
           throw new Error("Failed to fetch data");
